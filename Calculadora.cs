5cb16d872b62ec02bbc4ea78b483908f5625e1e4
robbfreitas <robertocompromissos@gmail.com>
2022-12-22 13:07:43 -0300

    Add project files.

---
+++ Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraComParams
+{
+    internal class Calculadora
+    {
+        // params informa que serão aceitos n parâmetros
+       public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace CalculadoraComParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s1 = Calculadora.Sum(2, 4, 10, 4);
+            Console.WriteLine(s1);
+        }
+    }
+}
